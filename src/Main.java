4535a5e84450598c8f0f0c0cf1dd089c003ed607
Isaac Forsmann <you@example.com>
2022-05-24 14:14:07 -0700

    First Window, YAY

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("Launching ...");
+        JFrame f = new JFrame();
+        f.setSize(500, 400);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
