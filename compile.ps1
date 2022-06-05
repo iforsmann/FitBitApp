d23b5f109d82f1b171c583611f6cbf758b3739ba
iforsmann <iforsmann@frontier.com>
2022-06-04 23:07:55 -0700

    Fixed compile and run scripts

--- compile.ps1
+++ compile.ps1
@@ -1,1 +1,1 @@
-javac ./src/*.java
+javac -cp ".;src;lib/jfreechart-1.0.1/lib/jcommon-1.0.0.jar;lib/jfreechart-1.0.1/lib/jfreechart-1.0.1.jar" src/*.java
--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-java -cp ./src Main
+java -cp ".;src;lib/jfreechart-1.0.1/lib/jcommon-1.0.0.jar;lib/jfreechart-1.0.1/lib/jfreechart-1.0.1.jar" Main
