27c883e573427d092b6b6f25eb31d53ac460070a
iforsmann <iforsmann@frontier.com>
2022-06-05 15:22:29 -0700

    added .env support

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,11 +1,13 @@
 import java.sql.*;
+import io.github.cdimascio.dotenv.*;
 
 public class DBConnection {
     private Connection con;
 
     public DBConnection(){
+        Dotenv dotenv = Dotenv.configure().load();
         try {
-            this.con = DriverManager.getConnection("jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/iforsman", "iforsman", "25759532");
+            this.con = DriverManager.getConnection(dotenv.get("CON_STRING"), dotenv.get("USER"), dotenv.get("PASSWORD"));
             Statement s = this.con.createStatement();
             ResultSet rs = s.executeQuery("Select * from Customer limit 5;");
             while(rs.next()){
